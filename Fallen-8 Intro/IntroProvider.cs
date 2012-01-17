b644e55eff120c046551896422d17477095f41ec
Henning Rauch <home@dreiundzwanzig.biz>
2012-01-17 19:55:25 +0100

    adds some basic creation/linking/traversal

---
+++ Fallen-8 Intro/IntroProvider.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fallen8.API;
+using System.Threading.Tasks;
+using Fallen8.API.Helper;
+using Fallen8.Model;
+
+namespace Intro
+{
+    class IntroProvider
+    {
+        private IFallen8 _fallen8;
+        private long _edgePropertyId;
+
+        public IntroProvider(IFallen8 fallen8)
+        {
+            _fallen8 = fallen8;
+            _edgePropertyId = 0L;
+        }
+
+        internal void CreateScaleFreeNetwork(int nodeCound, int edgeCount)
+        {
+            DateTime creationDate = DateTime.Now;
+            List<Int64> vertexIDs = new List<long>();
+            Random prng = new Random();
+
+            for (int i = 0; i < nodeCound; i++)
+            {
+                vertexIDs.Add(
+                    _fallen8.CreateVertex(
+                        new VertexModelDefinition(creationDate)
+                            .AddProperty(23, "Name" + i)
+                            ).Id);
+            }
+
+            foreach (var aVertexId in vertexIDs)
+            {
+                HashSet<Int64> targetVertices = new HashSet<long>();
+
+                do
+                {
+                    targetVertices.Add(vertexIDs[prng.Next(0, vertexIDs.Count)]);
+                } while (targetVertices.Count < edgeCount);
+
+                foreach (var aTargetVertex in targetVertices)
+                {
+                    _fallen8.CreateEdge(aVertexId, _edgePropertyId, new EdgeModelDefinition(aTargetVertex, creationDate)
+                        .AddProperty(42, aTargetVertex % 42));
+                }
+            }
+        }
+
+        internal void TraverseABit()
+        {
+            var vertex = _fallen8.Graph.GetVertices().First();
+
+
+            #region out edges
+
+            IEdgePropertyModel edgeProperty;
+            if (vertex.TryGetOutEdge(out edgeProperty, _edgePropertyId))
+            {
+                foreach (var aTargetVertex in edgeProperty.Select(_ => _.TargetVertex))
+                {
+                    // target vertices
+                }
+
+                foreach (var aEdge in edgeProperty)
+                {
+                    // edge
+                }
+            } 
+            
+            #endregion
+
+            #region inc edges
+
+            IEnumerable<IEdgeModel> incomingEdges;
+            if (vertex.TryGetInEdges(out incomingEdges, _edgePropertyId))
+            {
+                foreach (var aIncomingVertex in incomingEdges.Select(_ => _.SourceEdgeProperty.SourceVertex))
+                {
+                    // incoming vertices
+                }
+
+                foreach (var aIncomingVertex in incomingEdges.Select(_ => _.SourceEdgeProperty).SelectMany(__ => __))
+                {
+                    // outgoing vertices of the incoming vertices
+                }
+            }
+            
+            #endregion
+        }
+    }
+}
--- Fallen-8 Intro/Program.cs
+++ Fallen-8 Intro/Program.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
-namespace Fallen_8_Intro
+namespace Intro
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var fallen8 = new Fallen8.API.Fallen8();
+            var introProvicer = new IntroProvider(fallen8);
+            int nodeCount = 100000;
+            int edgeCount = 10;
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            introProvicer.CreateScaleFreeNetwork(nodeCount, edgeCount);
+            sw.Stop();
+            Console.WriteLine(String.Format("It took {0}ms to create a Fallen-8 graph with {1} nodes and {2} edges per node.", sw.Elapsed.TotalMilliseconds, nodeCount, edgeCount));
+
+            introProvicer.TraverseABit();
+
+            Console.WriteLine("done");
+            Console.ReadLine();
         }
     }
 }
