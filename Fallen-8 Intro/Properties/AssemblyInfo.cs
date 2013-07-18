4e982c74bd080657b357e3572a323f75cf0199ed
Henning Rauch <Henning@RauchEntwicklung.biz>
2013-07-18 10:57:34 +0200

    update to .net4.5

--- Fallen-8 Intro/Properties/AssemblyInfo.cs
+++ Fallen-8 Intro/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fallen-8 Intro")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An intro for Fallen-8")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fallen-8 Intro")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Henning Rauch 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
