e8eafc95664e28b660932321ac6be37197ce9e37
Henning Rauch <Henning@RauchEntwicklung.biz>
2014-05-01 18:45:17 +0200

    minor changes

--- Fallen-8 Intro/IntroProvider.cs
+++ Fallen-8 Intro/IntroProvider.cs
@@ -13,10 +13,9 @@ using NoSQL.GraphDB.Model;
 namespace Intro
 {
 	public class IntroProvider
-    {
-
+	{
 		private List<VertexModel> _toBeBenchenVertices = null;
-		private int _numberOfToBeTestedVertices = 10000000; 
+		private int _numberOfToBeTestedVertices = 10000000;
 		private Fallen8 _f8;
 
 		public IntroProvider (Fallen8 fallen8)
@@ -24,25 +23,24 @@ namespace Intro
 			_f8 = fallen8;
 		}
 
-        /// <summary>
-        /// Creates a scale free network
-        /// </summary>
-        /// <param name="nodeCound"></param>
-        /// <param name="edgeCount"></param>
-        /// <param name="fallen8"></param>
-        public void CreateScaleFreeNetwork(int nodeCound, int edgeCount)
-        {
-            var creationDate = DateHelper.ConvertDateTime(DateTime.Now);
-            var vertexIDs = new List<Int32>();
-            var prng = new Random();
+		/// <summary>
+		/// Creates a scale free network
+		/// </summary>
+		/// <param name="nodeCound"></param>
+		/// <param name="edgeCount"></param>
+		/// <param name="fallen8"></param>
+		public void CreateScaleFreeNetwork (int nodeCound, int edgeCount)
+		{
+			var creationDate = DateHelper.ConvertDateTime (DateTime.Now);
+			var vertexIDs = new List<Int32> ();
+			var prng = new Random ();
 			if (nodeCound < _numberOfToBeTestedVertices) {
 				_numberOfToBeTestedVertices = nodeCound;
 			}
 
 			_toBeBenchenVertices = new List<VertexModel> (_numberOfToBeTestedVertices);
 
-            for (var i = 0; i < nodeCound; i++)
-            {
+			for (var i = 0; i < nodeCound; i++) {
 //                vertexIDs.Add(
 //                    fallen8.CreateVertex(creationDate, new PropertyContainer[4]
 //                                                           {
@@ -51,40 +49,39 @@ namespace Intro
 //                                                               new PropertyContainer { PropertyId = 25, Value = "Ein kurzes Property" },
 //                                                               new PropertyContainer { PropertyId = 26, Value = "Ein gaaaaaaaanz langes Property" },
 //                                                           }).Id);
-				vertexIDs.Add(_f8.CreateVertex(creationDate).Id);
+				vertexIDs.Add (_f8.CreateVertex (creationDate).Id);
                         
-            }
+			}
 
-            foreach (var aVertexId in vertexIDs)
-            {
-                var targetVertices = new HashSet<Int32>();
+			if (edgeCount != 0) {
+				foreach (var aVertexId in vertexIDs) {
+					var targetVertices = new HashSet<Int32> ();
 
-                do
-                {
-                    targetVertices.Add(vertexIDs[prng.Next(0, vertexIDs.Count)]);
-                } while (targetVertices.Count < edgeCount);
+					do {
+						targetVertices.Add (vertexIDs [prng.Next (0, vertexIDs.Count)]);
+					} while (targetVertices.Count < edgeCount);
 
-                foreach (var aTargetVertex in targetVertices)
-                {
+					foreach (var aTargetVertex in targetVertices) {
 //                    fallen8.CreateEdge(aVertexId, 0, aTargetVertex, creationDate, new PropertyContainer[2]
 //                                                           {
 //                                                               new PropertyContainer { PropertyId = 29, Value = 23.4 },
 //                                                               new PropertyContainer { PropertyId = 1, Value = 2 },
 //                                                           });
 //
-					_f8.CreateEdge(aVertexId, 0, aTargetVertex, creationDate);
-                }
-            }
+						_f8.CreateEdge (aVertexId, 0, aTargetVertex, creationDate);
+					}
+				}
 
-			_toBeBenchenVertices.AddRange (PickInterestingIDs (vertexIDs, prng)
+				_toBeBenchenVertices.AddRange (PickInterestingIDs (vertexIDs, prng)
 				.Select (aId => {
 					VertexModel v = null;
 
-					_f8.TryGetVertex(out v, aId);
+					_f8.TryGetVertex (out v, aId);
 
 					return v;
-			}));
-        }
+				}));
+			}
+		}
 
 		IEnumerable<int> PickInterestingIDs (List<int> vertexIDs, Random prng)
 		{
@@ -93,84 +90,77 @@ namespace Intro
 			}
 		}
 
-        /// <summary>
-        /// Benchmark
-        /// </summary>
-        /// <param name="fallen8"></param>
-        /// <param name="myIterations"></param>
-        /// <returns></returns>
-		public String Bench(int myIterations = 1000)
-        {
+		/// <summary>
+		/// Benchmark
+		/// </summary>
+		/// <param name="fallen8"></param>
+		/// <param name="myIterations"></param>
+		/// <returns></returns>
+		public String Bench (int myIterations = 1000)
+		{
 			if (_toBeBenchenVertices == null) {
 				return "No vertices available";
 			}
 
 			List<VertexModel> vertices = _toBeBenchenVertices;
-            var tps = new List<double>();
-            long edgeCount = 0;
-            var sb = new StringBuilder();
+			var tps = new List<double> ();
+			long edgeCount = 0;
+			var sb = new StringBuilder ();
 			
-			Int32 range = ((vertices.Count/Environment.ProcessorCount)*3)/2;
+			Int32 range = ((vertices.Count / Environment.ProcessorCount) * 3) / 2;
 			
-            for (var i = 0; i < myIterations; i++)
-            {
-                var sw = Stopwatch.StartNew();
+			for (var i = 0; i < myIterations; i++) {
+				var sw = Stopwatch.StartNew ();
 
-                edgeCount = CountAllEdgesParallelPartitioner(vertices, range);
+				edgeCount = CountAllEdgesParallelPartitioner (vertices, range);
 
-                sw.Stop();
+				sw.Stop ();
 
-                tps.Add(edgeCount / sw.Elapsed.TotalSeconds);
-            }
+				tps.Add (edgeCount / sw.Elapsed.TotalSeconds);
+			}
 
-            sb.AppendLine(String.Format("Traversed {0} edges. Average: {1}TPS Median: {2}TPS StandardDeviation {3}TPS ", edgeCount, Statistics.Average(tps), Statistics.Median(tps), Statistics.StandardDeviation(tps)));
+			sb.AppendLine (String.Format ("Traversed {0} edges. Average: {1}TPS Median: {2}TPS StandardDeviation {3}TPS ", edgeCount, Statistics.Average (tps), Statistics.Median (tps), Statistics.StandardDeviation (tps)));
 
-            return sb.ToString();
-        }
+			return sb.ToString ();
+		}
 
-        /// <summary>
-        /// Counter
-        /// </summary>
-        /// <param name="vertices"></param>
-        /// <param name="vertexRange"></param>
-        /// <returns></returns>
-        private static long CountAllEdgesParallelPartitioner(List<VertexModel> vertices, Int32 vertexRange)
-        {
-            var lockObject = new object();
-            var edgeCount = 0L;
-            var rangePartitioner = Partitioner.Create(0, vertices.Count, vertexRange);
+		/// <summary>
+		/// Counter
+		/// </summary>
+		/// <param name="vertices"></param>
+		/// <param name="vertexRange"></param>
+		/// <returns></returns>
+		private static long CountAllEdgesParallelPartitioner (List<VertexModel> vertices, Int32 vertexRange)
+		{
+			var lockObject = new object ();
+			var edgeCount = 0L;
+			var rangePartitioner = Partitioner.Create (0, vertices.Count, vertexRange);
 
-            Parallel.ForEach(
-                rangePartitioner,
-                () => 0L,
-                delegate(Tuple<int, int> range, ParallelLoopState loopstate, long initialValue)
-                    {
-                        var localCount = initialValue;
+			Parallel.ForEach (
+				rangePartitioner,
+				() => 0L,
+				delegate(Tuple<int, int> range, ParallelLoopState loopstate, long initialValue) {
+					var localCount = initialValue;
 
-                        for (var i = range.Item1; i < range.Item2; i++)
-                        {
-                            ReadOnlyCollection<EdgeModel> outEdge;
-                            if (vertices[i].TryGetOutEdge(out outEdge, 0))
-                            {
-                                for (int j = 0; j < outEdge.Count; j++)
-                                {
-                                    var vertex = outEdge[j].TargetVertex;
-                                    localCount++;
-                                }
-                            }
-                        }
+					for (var i = range.Item1; i < range.Item2; i++) {
+						ReadOnlyCollection<EdgeModel> outEdge;
+						if (vertices [i].TryGetOutEdge (out outEdge, 0)) {
+							for (int j = 0; j < outEdge.Count; j++) {
+								var vertex = outEdge [j].TargetVertex;
+								localCount++;
+							}
+						}
+					}
 
-                        return localCount;
-                    },
-                delegate(long localSum)
-                    {
-                        lock (lockObject)
-                        {
-                            edgeCount += localSum;
-                        }
-                    });
+					return localCount;
+				},
+				delegate(long localSum) {
+					lock (lockObject) {
+						edgeCount += localSum;
+					}
+				});
 
-            return edgeCount;
-        }
-    }
+			return edgeCount;
+		}
+	}
 }
